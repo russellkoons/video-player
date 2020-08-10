3803daf84bae5a230331cb7fcea70901bb9f50cc
russellkoons <rkoons@gmail.com>
2020-08-10 06:53:05 -0400

    skip

--- scripts.js
+++ scripts.js
@@ -23,9 +23,14 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
